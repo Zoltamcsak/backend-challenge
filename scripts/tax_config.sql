19d4e7dd163586efec4eb5fe51d0d5fef9fe8149
Tamas Zolcsak <tamas.zolcsak@globant.com>
2022-02-20 20:41:46 +0200

    add sql script for adding data

---
+++ scripts/tax_config.sql
@@ -0,0 +1,11 @@
+insert into tax_configs (country, tax_name, tax_percentage_value) VALUES ('ITALY', 'tax', 25);
+insert into tax_configs (country, tax_name, tax_percentage_value) VALUES ('ITALY', 'health_insurance', 7);
+insert into tax_configs (country, tax_name, tax_percentage_value) VALUES ('FRANCE', 'tax', 30);
+
+insert into extra_salaries (country, month) VALUES ('ITALY', 8);
+insert into extra_salaries (country, month) VALUES ('ITALY', 12);
+
+insert into user_profiles (first_name, last_name, profile_picture_url) values ('Joe', 'Don', 'www.profile.com');
+insert into user_profiles (first_name, last_name, profile_picture_url) values ('Michael', 'James', 'www.profile.com');
+insert into user_profiles (first_name, last_name, profile_picture_url) values ('Steph', 'Curry', 'www.profile.com');
+insert into user_profiles (first_name, last_name, profile_picture_url) values ('Kevin', 'Durant', 'www.profile.com');
